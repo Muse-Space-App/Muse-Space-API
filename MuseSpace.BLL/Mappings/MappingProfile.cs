4fb0642f6eab121e8e12079fbbce4c66b0e4695b
melvin12129986 <melvinwijaya2017@gmail.com>
2026-06-12 15:55:19 +0700

    test fix comment

--- MuseSpace.BLL/Mappings/MappingProfile.cs
+++ MuseSpace.BLL/Mappings/MappingProfile.cs
@@ -8,6 +8,11 @@ public sealed class MappingProfile : Profile
 {
     public MappingProfile()
     {
+        CreateMap<Comment, CommentResponse>();
+
+        CreateMap<List<Comment>, IReadOnlyCollection<CommentResponse>>()
+            .ConvertUsing((src, _, context) => context.Mapper.Map<List<CommentResponse>>(src).AsReadOnly());
+            
         CreateMap<BaseEntity, SampleDto>()
             .ForMember(destination => destination.Name, options => options.MapFrom(source => source.CreatedBy));
     }
